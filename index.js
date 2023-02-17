9e40e6c79cc54b4c36f78033294d6ea7dc39bf07
GAYATHRI-3200 <subbusubramani1995@gmail.com>
2023-02-17 16:54:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  // Write your code here.
+  <div className="container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="img"
+      />
+      <h1 className="title">Male</h1>
+      <p className="caption">Bangalore University, Bangalore</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
